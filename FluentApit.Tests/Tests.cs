624bd95a73537877fd3e91c08de2548fc2f94a98
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-09-05 14:51:27 +0200

    some test stuff

--- FluentApit.Tests/Tests.cs
+++ FluentApit.Tests/Tests.cs
@@ -76,10 +76,49 @@ namespace FluentApit.Tests
             bool projectHasTeam = team.Name == t.Name && affiliatedProject.Name == p.Name;
             Assert.IsTrue(projectHasTeam);
         }
+        [ExpectedException(typeof(Exception),AllowDerivedTypes = true)]
         [TestMethod]
         public void InvalidOperationExceptionOnCreateUnaffiliatedContactInfo()
         {
+            ContactInfo contactInfo = new ContactInfo();
+            contactInfo.Email = "Mara@aspit.dk";
+            contactInfo.Phone = "12345678";
+            Model model = new Model();
+            model.ContactInfos.Add(contactInfo);
+            model.SaveChanges();
+        }
 
+        [TestMethod]
+        public void CreateEmployeeWithoutContactInfo()
+        {
+            Model model = new Model();
+            string newName = "martin";
+            Employee newEmployee = new Employee { Name = newName };
+            model.Employees.Add(newEmployee);
+            
+            
+            model.SaveChanges();
+            Employee existingEmployee = model.Employees.Single(e => e.Name == newName);
+
+            Assert.AreEqual(newEmployee.Name, existingEmployee.Name);
+
+        }
+        [TestMethod]
+        public void CreateEmployeeWithContactInfo()
+        {
+            Model model = new Model();
+            string newName = "test2name";
+            Employee newEmployee = new Employee { Name = newName };
+            ContactInfo newContactInfo = new ContactInfo { Email = "test2mail@aspit.dk", Phone = "12344321" };
+            newEmployee.ContactInfo = newContactInfo;
+            model.Employees.Add(newEmployee);
+
+            model.SaveChanges();
+            Employee existingEmployee = model.Employees.Single(e => e.Name == newName);
+            ContactInfo existingContactInfo = existingEmployee.ContactInfo;
+
+            Assert.AreEqual(newEmployee.Name, existingEmployee.Name);
+            Assert.AreEqual(newContactInfo.Phone, existingContactInfo.Phone);
         }
     }
 }
