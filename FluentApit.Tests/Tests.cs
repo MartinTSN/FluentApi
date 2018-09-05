75b59e6ce2c11aa757f2aae319100401ef7778f0
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-09-05 11:00:16 +0200

    made another test method

--- FluentApit.Tests/Tests.cs
+++ FluentApit.Tests/Tests.cs
@@ -13,10 +13,24 @@ namespace FluentApit.Tests
         public void GetAllProjects()
         {
             Model model = new Model();
-            var results = model.Projects;
+            var results = model.Projects.ToList();
 
+            Assert.AreNotEqual(results.Count, 0);
 
+        }
+        [TestMethod]
+        public void AddNewProject()
+        {
+            Model model = new Model();
+            Project p = new Project();
+            p.Name = "Jens";
+            p.Description = "Jens's Store";
+            model.Projects.Add(p);
+            int count = model.Projects.ToList().Count;
+            model.SaveChanges();
+            int newcount = model.Projects.ToList().Count;
 
+            Assert.AreEqual(newcount, count + 1);
         }
     }
 }
