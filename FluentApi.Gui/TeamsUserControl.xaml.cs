1e4c8fc6723b6cc60e6718b49115d2750b93f621
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-09-11 09:59:20 +0200

    made add/remove buttons and some functionality

--- FluentApi.Gui/TeamsUserControl.xaml.cs
+++ FluentApi.Gui/TeamsUserControl.xaml.cs
@@ -28,28 +28,91 @@ namespace FluentApi.Gui
         {
             InitializeComponent();
             model = new Model();
-            dataGridTeams.ItemsSource = model.Teams.ToList();
-
+            ReloadDataGridTeams();
+            ReloadDataGridEmployees();
         }
 
         private void DataGrid_Teams_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             selectedTeam = dataGridTeams.SelectedItem as Team;
-            dataGridEmployees.ItemsSource = selectedTeam.Employees;
-            buttonShowAllEmployees.IsEnabled = true;
-            buttonShowOnTeam.IsEnabled = true;
-
+            if (selectedEmployee != null)
+            {
+                buttonAddToTeam.IsEnabled = true;
+            }
+            else
+            {
+                if (selectedTeam == null)
+                {
+                    dataGridTeams.ItemsSource = null;
+                }
+                else
+                {
+                    dataGridEmployees.ItemsSource = selectedTeam.Employees;
+                }
+                dataGridTeams.SelectedItem = null;
+                ReloadDataGridTeams();
+            }
+        }
+        private void ReloadDataGridTeams()
+        {
+            dataGridTeams.ItemsSource = model.Teams.ToList();
         }
 
-        private void Button_ShowAllEmployees_Click(object sender, RoutedEventArgs e)
+        private void ReloadDataGridEmployees()
         {
             dataGridEmployees.ItemsSource = model.Employees.ToList();
         }
 
-        private void Button_ShowOnTeam_Click(object sender, RoutedEventArgs e)
+        private void Button_ShowAllEmployees_Click(object sender, RoutedEventArgs e)
         {
+            ReloadDataGridEmployees();
+            dataGridTeams.SelectedItem = null;
+            dataGridEmployees.SelectedItem = null;
+            buttonAddToTeam.IsEnabled = false;
+        }
+
+        private void DataGrid_Employees_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedEmployee = dataGridEmployees.SelectedItem as Employee;
+        }
+
+        private void Button_AddToTeam_Click(object sender, RoutedEventArgs e)
+        {
+            selectedEmployee = dataGridEmployees.SelectedItem as Employee;
             selectedTeam = dataGridTeams.SelectedItem as Team;
-            dataGridEmployees.ItemsSource = selectedTeam.Employees;
+
+            if (selectedEmployee != null && selectedTeam != null)
+            {
+                foreach (Employee employee in selectedTeam.Employees)
+                {
+                    if (selectedEmployee != employee)
+                    {
+                        selectedTeam.Employees.Add(selectedEmployee);
+                    }
+                }
+                model.SaveChanges();
+                ReloadDataGridTeams();
+                ReloadDataGridEmployees();
+            }
+        }
+
+        private void Button_RemoveFromTeam_Click(object sender, RoutedEventArgs e)
+        {
+            selectedEmployee = dataGridEmployees.SelectedItem as Employee;
+            selectedTeam = dataGridTeams.SelectedItem as Team;
+            if (selectedEmployee != null && selectedTeam != null)
+            {
+                foreach (Employee employee in selectedTeam.Employees)
+                {
+                    if (selectedEmployee == employee)
+                    {
+                        selectedTeam.Employees.Remove(selectedEmployee);
+                    }
+                }
+                model.SaveChanges();
+                ReloadDataGridTeams();
+                ReloadDataGridEmployees();
+            }
         }
     }
 }
